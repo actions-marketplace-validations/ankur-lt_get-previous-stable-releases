d167b3fd4f247415f2e2d24103d4eeeb3af8dc13
Ankur Narkhede <ankurn@lambdatest.com>
2021-08-27 09:36:45 +0530

    [MLE-5505] Added functionality for getting prepreleases too

--- dist/index.js
+++ dist/index.js
@@ -551,19 +551,19 @@ try {
     _.isArray(_.get(response, 'data')) && _.forEach(_.get(response, 'data'), (release) => {
       if ((stable === true && !release.prerelease) || (stable === false)) {
         if (!firstSet) {
-          core.setOutput('previousStableTag', release.tag_name)
-          console.log("Set previousStableTag as::", release.tag_name)
+          core.setOutput('previousTag', release.tag_name)
+          console.log("Set previousTag as::", release.tag_name)
           firstSet = true
         }
         else if(!secondSet) {
-          core.setOutput('previousSecondStableTag', release.tag_name)
-          console.log("Set previousSecondStableTag as::", release.tag_name)
+          core.setOutput('previousSecondTag', release.tag_name)
+          console.log("Set previousSecondTag as::", release.tag_name)
 
           secondSet = true
         }
         else if(!thirdSet) {
-          core.setOutput('previousThirdStableTag', release.tag_name)
-          console.log("Set previousThirdStableTag as::", release.tag_name)
+          core.setOutput('previousThirdTag', release.tag_name)
+          console.log("Set previousThirdTag as::", release.tag_name)
 
           thirdSet = true
         }
